7bdb9c8ba4eba83b6c6b7896dcdc0237da31e90f
sadiet20 <sadierosethomas@gmail.com>
2021-06-03 10:48:57 -0700

    added tests

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+"""
+Tests for Calculator App
+"""
+
+import calculator
+
+
+class TestCalculatorApp:
+	def test_add(self):
+		assert 5 == calculator.add(3, 2)
+	def test_subtract(self):
+		assert 5 == calculator.subtract(10, 5)
+
